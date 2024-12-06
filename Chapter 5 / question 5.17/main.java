28a361cba632945721e2a659a1b01fb472f58288
Halil <156548013+SkyFetch0@users.noreply.github.com>
2024-12-06 19:16:00 +0300

    Update main.java

--- Chapter 5 / question 5.17/main.java
+++ Chapter 5 / question 5.17/main.java
@@ -5,49 +5,55 @@ public class question517 {
 
     public static void main(String[] args) {
 
-        int number = 16;
-
-        String msg = "";
-
-        for(int i = 1; i <= number; i++) {
-
-
-            int ic = number - i;
-            String msc = "";
-            for(int a = 1; a <= ic; a++){
-                msc += "  ";
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number of lines: ");
+        int limit = scan.nextInt();
+            //En Sonda Ekrana Yazdırmak İçin Boş Bir String Değer Oluşturuyoruz
+        String Mesaj = "";
+        for(int i = 1; i <= limit; i++) {
+                // Her Satırda Kaç Tane Boşluk Olduğunu Bulmak İçin BoslukSayisi Değerini Oluşturuyoruz
+            int BoslukSayisi = limit - i;
+                // Boşlukları Yazacağımız String Değerimizi Oluşturuyoruz
+            String Bosluklar = "";
+            for(int a = 1; a <= BoslukSayisi; a++){
+                Bosluklar += "  ";
             }
-            int is = i;
-            String numbers = "";
-            for(int v = 0; v < is;) {
-                if(is <= 0) {
-                    continue;
-                }
-                numbers += " "+is + "";
-                is--;
+            // Piramidin Sol Tarafını Yapıyoruz
+            int iYedek = i;
+            String Sayilar = "";
+            for(int v = 0; v < iYedek;) {
+                // Sayılar 6 5 4 3 2 1 şeklinde gideceği için en büyükten yazdırmaya başalıyoruz
+                Sayilar += " "+iYedek + "";
+                // Git Gide Azaltıyoruz
+                iYedek--;
             }
 
 
-
+            // Piramidin Sağ Tarafını Yapıyoruz
             for(int j = 1; j <= i;) {
+                // if ile i Değerinin 1 olup olmadığını kontrol ediyoruz
+                // Burdaki Amaç Ortada 2 Tane 1 Olmasın Diye
                 if(i == 1) {
-                 numbers += "";
+                    // Eğer i Değeri 1 İse Sayilar Kısmını Boş Bırak
+                    Sayilar += "";
 
                 }else if(j == 1) {
-                    numbers += "";
+                    // if ile j Değerinin 1 olup olmadığını kontrol ediyoruz
+                    // Burdaki Amaç Ortada 2 Tane 1 Olmasın Diye
+                    // Eğer j Değeri 1 İse Sayilar Kısmını Boş Bırak
+                    Sayilar += "";
                 } else {
-                    numbers += " "+j + "";
+                    Sayilar += " "+j + "";
                 }
                 j++;
             }
-            msg += msc + numbers + "\n";
+            // Piramitin Yeni Satırlarını Mesaja Ekliyoruz
+            Mesaj += Bosluklar + Sayilar + "\n";
 
 
         }
-        System.out.println(msg);
-
-
-
+        // Döngü Bittikten Sonra Mesajı Yazdır
+        System.out.println(Mesaj);
     }
 
-    }
+}
